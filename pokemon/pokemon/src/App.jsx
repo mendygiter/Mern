bd05266fa27aaf454f978fcd487040fd5e0029cf
mendygiter <m.giter@fusegen.ai>
2022-07-13 16:18:48 -0400

    set up with axios

---
+++ pokemon/pokemon/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import PokeApi from "./components/PokeApi"
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Poke API </h1>
+
+        <PokeApi/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemon/pokemon/src/components/PokeApi.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const PokeApi = props => {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default PokeApi;
+
