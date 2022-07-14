a9e4f253c5c0dc6ae559379b3a786a55aa98d475
mendygiter <m.giter@fusegen.ai>
2022-07-14 17:01:44 -0400

    complete

--- pokemon/pokemon/src/components/PokeApi.jsx
+++ pokemon/pokemon/src/components/PokeApi.jsx
@@ -1,12 +1,27 @@
 import axios from "axios";
+import { useState, useEffect} from "react";
+
 
 const PokeApi = props => {
+    const [pokemon, setPokemon] = useState(null);
+    // useEffect(() => {
+    //     axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+    //         .then(res => setPokemon(res.data.results))
+    // }, [])
+
+    const handlePokemon = () => {
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+            .then(res => setPokemon(res.data.results))
+    }
+
     return(
         <div>
-
+            <button onClick={handlePokemon}>Fetch Pokemon</button>
+            {pokemon ? pokemon.map((results, index) => {
+                return(<div key={index}>{results.name}</div> )
+            }):null}
         </div>
     );
 }
 
 export default PokeApi;
-
